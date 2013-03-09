171f9db59f7df154aa5fa0f03a6cb5bf6e6f36c8
Steve Shogren <steve.a.shogren@gmail.com>
2013-03-09 12:32:22 -0500

    adding images to the drawing

--- src/art_work/handler.clj
+++ src/art_work/handler.clj
@@ -4,12 +4,13 @@
             [art-work.art :as art]
             [clojure.pprint :as pp]
             [hiccup.core :as h]
+            [hiccup.page :as p]
             [compojure.route :as route]))
 
 (defn make-row [[x y val]]
   (cond
-   (nil? val) [:td ""]
-   :else [:td {:class val}  val]))
+   (nil? val) [:td {:class "empty"} ""]
+   :else [:td {:class (str "num" val)}  ""]))
 
 (defn m-table [table]
   (map
@@ -20,12 +21,14 @@
 (defn pr [table]
   (h/html [:head
            [:title "A Large Set"]]
+          (p/include-css "style.css")
           [:body
            [:div
             [:table (m-table table)]]]))
 
 (defroutes app-routes
-  (GET "/" [] (str "Test: " (pr (art/generate 50))) )
+  (GET "/" [] (str "Test: " (pr (art/generate 50))))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
